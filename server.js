676be5ddf52be5e5e662723523f9f23bec9e5860
yeabsira-mesfin <yeabsira.mesfin29@gmail.com>
2024-04-15 07:44:21 -0400

    mongoDB

--- server.js
+++ server.js
@@ -1,7 +1,26 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
-const PORT = 3000;
-// created 
+const PORT = 5000;
+
+// connect to MondoDB
+mongoose.
+connect('mongodb+srv://Yeab:yeab@nodejscluster0.aiwdel2.mongodb.net/?retryWrites=true&w=majority&appName=nodejsCluster0').
+then(() => console.log('MongoDB connected')).
+catch(err => console.error('MongoDB connection error:',err));
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// Createing a Schema for messages
+const messageSchema = new mongoose.Schema({
+    text: String
+})  
+
+// Creating a model for messages
+const Message = mongoose.model('Yeabsira',messageSchema);
+
+// authentication
 const authenticate = (req,res,next) => {
     const {authorization} = req.headers;
     if(authorization && authorization.startsWith('Bearer ')){
@@ -13,14 +32,40 @@ const authenticate = (req,res,next) => {
     res.status(401).json({error: 'You are unauthorized'})
 }
 
-app.get('/api/protected', authenticate,(req,res) =>{
-    res.json({message: 'You are authorized to access this resource'})
+// Route to get all messages
+app.get('/api/messages',authenticate,async(req, res)=>{
+    try{
+        const messages = await Message.find();
+        res.json(messages);
+    } catch(err) {
+        res.status(500).json({error: err.message})
+    }
 })
 
 
-app.get('/api/hello',(req,res)=> {
-    res.json({message: 'This is API talking'})
-})
+
+// app.get('/api/protected', authenticate,(req,res) =>{
+//     res.json({message: 'You are authorized to access this resource'})
+// })
+
+
+// app.get('/api/hello',(req,res)=> {
+//     res.json({message: 'This is API talking'})
+// })
+
+// Route to add new message
+app.post('/api/messages', authenticate, async (req, res) => {
+    const { text } = req.body;
+    if (!text) {
+        return res.status(400).json({ error: 'Text is required' });
+    }
+    try {
+        const newMessage = await Message.create({ text });
+        res.status(201).json(newMessage);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
 
 app.listen(PORT,() => {
     console.log(`Server running on port ${PORT}`)
